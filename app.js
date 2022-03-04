b2cf3f759e97b3fd400c88b454165e59be0340a7
Pvag <paolondon@gmail.com>
2022-03-04 18:06:09 +0000

    domanda 1: 1 solo tick per ogni nome

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+// dove devo spostare le due righe seguenti, per far si che
+// compaia un (ed uno solo) tick-mark ad ogni nome sul quale clicco?
 const checkMark = document.createElement('span')
 checkMark.innerHTML = ' <i class="fa-solid fa-check"></i>'
 // recupera tutti gli elementi h1 che siano dentro un elemento di classe .box
 // (immediati discendenti (figli), grazie al >, non discendenti qualsiasi (nipoti, pronipoti, ecc.))
 const elements = document.querySelectorAll('.box > h1')
 
-// per ciacun h1 che sta in un elemento con classe box
+// per ciascun h1 che sta in un elemento con classe box
 elements.forEach(element => {
   // aggiungi un event listener che, al click del mouse,
   // appenda il simbolo del "check-box" (preso da font-awesome)
